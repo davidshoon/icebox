2e160e82ad5aedf0e5353e216d1c50412c38ccfa
David Shoon <18284887+davidshoon@users.noreply.github.com>
2020-03-18 17:39:29 +1100

    Moved "de-duplication program" files from originals/ to de-dup/

--- de-dup/compile.sh
+++ de-dup/compile.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-g++ -o blockchain.out blockchain.cpp -lcrypto
-
 g++ -o recursive_sha256sum.out recursive_sha256sum.cpp -lcrypto
 
 g++ -o delete_and_symlink_dup_shasum.out delete_and_symlink_dup_shasum.cpp
